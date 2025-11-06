7d0c37ba8b31f38e965c378262173f68271344c8
Deivi04 <silver27031969@gmail.com>
2025-11-06 18:16:10 +0100

    Add files via upload

--- ejemplo.c
+++ ejemplo.c
@@ -23,15 +23,64 @@ int main() {
         token = strtok(linea, ",");
         if (token != NULL) {
             // Imprime el primer valor (columna 1)
-            printf("Columna 1: %s\n", token);
+            printf("Código: %s\n", token);
             
             // Obtiene y convierte los siguientes valores
             token = strtok(NULL, ","); // Columna 2
             if (token != NULL) {
                 int valor_columna2 = atoi(token);
-                printf("Columna 2: %d\n", valor_columna2);
+                printf("IQ: %d\n", valor_columna2);
+                
             }
-            // ... repite para las otras columnas
+            
+            token = strtok(NULL, ","); // Columna 3
+            if (token != NULL) {
+                float valor_columna3 = atof(token);
+                printf("Reultado previo trimestre: %f\n", valor_columna3);
+            }
+            
+            token = strtok(NULL, ","); // Columna 4
+            if (token != NULL) {
+                float valor_columna4 = atof(token);
+                printf("CGPA: %f\n", valor_columna4);
+            }
+            
+            token = strtok(NULL, ","); // Columna 5
+            if (token != NULL) {
+                int valor_columna5 = atoi(token);
+                printf("Rendimiento académico: %d\n", valor_columna5);
+            }
+            
+            token = strtok(NULL, ","); // Columna 6
+            if (token != NULL) {
+                
+                printf("Becado: %s\n", token);
+            }
+            
+            token = strtok(NULL, ","); // Columna 7
+            if (token != NULL) {
+                int valor_columna7 = atoi(token);
+                printf("Puntuación extracurricular: %d\n", valor_columna7);
+            }
+            
+            token = strtok(NULL, ","); // Columna 8
+            if (token != NULL) {
+                int valor_columna8 = atoi(token);
+                printf("Habilidades de comunicación: %d\n", valor_columna8);
+            }
+            
+            token = strtok(NULL, ","); // Columna 9
+            if (token != NULL) {
+                int valor_columna9 = atoi(token);
+                printf("Proyectos completados: %d\n", valor_columna9);
+            }
+            
+            token = strtok(NULL, ","); // Columna 10
+            if (token != NULL) {
+                
+                printf("Trabajo conseguido: %s\n", token);
+            }
+            
         }
     }
 
